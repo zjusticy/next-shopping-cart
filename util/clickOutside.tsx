b1cbf15af1042250d38d8d6ad6305b483ec4d48b
zjusticy <zhutong@justice.hitronhub.home>
2020-10-06 21:20:13 -0700

    Layout for mobile device fixed

--- util/clickOutside.tsx
+++ util/clickOutside.tsx
@@ -5,26 +5,26 @@ const useClickOutside = (
   ref: React.RefObject<HTMLDivElement>,
   activeCheck: boolean
 ) => {
-  const handleClickOutside = (e: MouseEvent) => {
-    if (activeCheck && ref.current.classList.contains("active")) {
-      if (!ref || !ref.current.contains(e.target as Element)) {
-        // inside click
-        closeModel();
-        e.stopPropagation();
-      }
-    } else if (!ref || !ref.current.contains(e.target as Element)) {
-      closeModel();
-    }
-  };
-
   useEffect(() => {
+    const handleClickOutside = (e: MouseEvent) => {
+      if (activeCheck && ref.current.classList.contains("active")) {
+        if (!ref || !ref.current.contains(e.target as Element)) {
+          // inside click
+          closeModel();
+          e.stopPropagation();
+        }
+      } else if (!ref || !ref.current.contains(e.target as Element)) {
+        closeModel();
+      }
+    };
+
     // add when mounted
     document.addEventListener("click", handleClickOutside, true);
     // return function to be called when unmounted
     return () => {
       document.removeEventListener("click", handleClickOutside, true);
     };
-  }, []);
+  }, [activeCheck, closeModel, ref]);
 };
 
 export default useClickOutside;
